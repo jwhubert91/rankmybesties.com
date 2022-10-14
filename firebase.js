c957cd69239d35ab18b65f66fab6c4bc5825b48e
James Hubert <jwhubert91@gmail.com>
2022-10-14 18:51:03 -0400

    properly load in the env vars

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAuth, TwitterAuthProvider } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_API_KEY,
-  authDomain: NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: NEXT_PUBLIC_APP_ID,
-  measurementId: NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
